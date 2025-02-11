73dbd21d630b82b406b774ea0bcc85ff40c74afa
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-02-11 08:36:34 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,34 @@
 
 document.addEventListener('click', (e) => {
   const spider = document.querySelector('.spider'); // Отримання павука;
-
-  const clickX = e.clientX + window.scrollX; // Отримання координат кліку
-  const clickY = e.clientY + window.ScrollY;
-
-  const spiderRect = spider.getBoundingClientRect(); // Розмір павука
   const wall = document.querySelector('.wall'); // Отримуємо стіну
+
+  const clickX = e.clientX; // Отримання координат кліку
+  const clickY = e.clientY;
+
   const wallRect = wall.getBoundingClientRect(); // Розмір стіни
 
+  // Перевірка на клік в межах стіни
+  if (
+    clickX < wallRect.left ||
+    clickX > wallRect.right ||
+    clickY < wallRect.top ||
+    clickY > wallRect.bottom
+  ) {
+    return;
+  }
+
   // Максимальні координати для павука
-  const maxX = wallRect.right - spiderRect.width / 2; // Макс координата X
-  const minX = wallRect.left + spiderRect.width / 2; // Мінімальна координата X
-  const maxY = wallRect.bottom - spiderRect.height / 2; // Макс координата Y
-  const minY = wallRect.top + spiderRect.height / 2; // Мінімальна координата Y
+  const maxX = wallRect.width - spider.offsetWidth / 2; // Макс координата X
+  const minX = wallRect.left + spider.offsetWidth / 2; // Мін координата X
+  const maxY = wallRect.height - spider.offsetHeight / 2; // Макс координата Y
+  const minY = wallRect.top + spider.offsetHeight / 2; // Мін координата Y
 
   // Нові координати для павука
   const newX = Math.min(Math.max(clickX, minX), maxX);
   const newY = Math.min(Math.max(clickY, minY), maxY);
 
   // Переміщення павука
-  spider.style.left = `${newX - spiderRect.width / 2}px`;
-  spider.style.top = `${newY - spiderRect.height / 2}px`;
+  spider.style.left = `${newX - spider.offsetWidth / 2}px`;
+  spider.style.top = `${newY - spider.offsetHeight / 2}px`;
 });
